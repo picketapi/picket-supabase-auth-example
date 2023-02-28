5c05c478cc6a0f0541e81ba2793743c248b02efc
Devin Stein <devstein@seas.upenn.edu>
2023-02-27 18:50:51 -0800

    feat: use rainbowkit integration for login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,49 @@ import type { AppProps } from "next/app";
 
 import { PicketProvider } from "@picketapi/picket-react";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import * as chain from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { PicketRainbowAuthProvider } from "@picketapi/picket-react";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "",
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Picket + Supabase",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PicketProvider apiKey={process.env.NEXT_PUBLIC_PICKET_PUBLISHABLE_KEY!}>
-      <Component {...pageProps} />
-    </PicketProvider>
+    <WagmiConfig client={wagmiClient}>
+      <PicketRainbowAuthProvider
+        apiKey={process.env.NEXT_PUBLIC_PICKET_PUBLISHABLE_KEY!}
+      >
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </PicketRainbowAuthProvider>
+    </WagmiConfig>
   );
 }
